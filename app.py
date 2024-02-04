6bfba8f00ef40d1e414316036ce8f1a630c6eda5
Hillary Fotino <casscainbatgirl@gmail.com>
2024-02-03 21:49:28 -0500

    Added caveats by name

--- app.py
+++ app.py
@@ -19,10 +19,6 @@ def get_version():
 def get_all_spells():
     return jsonify({'spells': spells})
 
-@app.route('/antioch/api/v1.0/caveats', methods=['GET'])
-def get_all_caveats():
-    return jsonify({'caveats': caveats})
-
 @app.route('/antioch/api/v1.0/spell/<string:spell_name>', methods=['GET'])
 def get_spells_by_name(spell_name):
     spell = [spell for spell in spells if spell['name'] == spell_name]
@@ -30,19 +26,30 @@ def get_spells_by_name(spell_name):
         abort(404)
     return jsonify({'spells': spell[0]})
 
-@app.route('/antioch/api/v1.0/spell_caveats/<string:spell_name>', methods=['GET'])
-def get_caveats_for_spell(spell_name):
-    spell = [spell for spell in spells if spell['name'] == spell_name]
-    if len(spell) == 0:
-        abort(404)
-    return jsonify({'spells': spell[0]["caveats"]})
-
 @app.route('/antioch/api/v1.0/spells_by_circle/<int:circle>', methods=['GET'])
 def get_spell_by_circle(circle):
     spell = [spell for spell in spells if spell['circle'] == circle]
     if len(spell) == 0:
         abort(404)
     return jsonify({'spells': spell})
+
+@app.route('/antioch/api/v1.0/caveats', methods=['GET'])
+def get_all_caveats():
+    return jsonify({'caveats': caveats})
+
+@app.route('/antioch/api/v1.0/caveats', methods=['GET'])
+def get_caveat_by_name(caveat_name):
+    spell = [caveat for caveat in caveats if caveat['name'] == caveat_name]
+    if len(spell) == 0:
+        abort(404)
+    return jsonify({'caveats': spell[0]})
+
+@app.route('/antioch/api/v1.0/spell_caveats/<string:spell_name>', methods=['GET'])
+def get_caveats_for_spell(spell_name):
+    spell = [spell for spell in spells if spell['name'] == spell_name]
+    if len(spell) == 0:
+        abort(404)
+    return jsonify({'spells': spell[0]["caveats"]})
 
 if __name__ == '__main__':
     app.run(port=os.getenv("PORT", default=5000), host='0.0.0.0')
