c943efe2e93d4e12decb58e889a810c2b760b6fb
Hillary Fotino <casscainbatgirl@gmail.com>
2024-02-03 21:58:22 -0500

    Fixed formatting for spell caveat names

--- data.py
+++ data.py
@@ -1,78 +1,78 @@
 caveats = [
     {
-        "name": "OOC Calls",
+        "name": "ooc calls",
         "desc": "These are special OOC calls to inform and describe specific IC game effects, These calls do not break verbals, chants or other VCs as they are purely game mechanics and martialing tools. All spells that utilize special combat calls describe the specific call that must be made with its use."
     },
     {
-        "name": "Chanting",
+        "name": "chanting",
         "desc": 'Some spells require that their VC be chanted continuously for the duration of the spell. These spells do not take effect until the VC has been recited fully, at least once. These spells last as long as the spellcaster continues the chant. OOC explanations (such as combat calls) do not interrupt these spells. For example, if a PC is chanting a Transmute Self spell and is hit by a weapon, they may call “No effect” without interrupting the spell; if a PC is chanting a Ward: Undead spell and a goblin hits their leg, they can call “Leg” (or “Armor,” or “Armored Cloak,” etc.) without having the spell end. The VC for these spells must be spoken clearly and loudly enough that anyone affected by the spell can understand them. Chanting spells can be disrupted by the 4th circle spell Disrupt. It is the PC’s responsibility to know what the Disrupt spell is, how to recognize it, and how to respond to it.'
     },
     {
-        "name": "Circles",
+        "name": "circles",
         "desc": "There are a number of spells that are considered circle spells. A circle spell must be clearly defined by a length of rope that has been laid on the ground with the ends overlapping. The ends cannot be tied together or secured in any way, and the rope in general cannot be secured or bound in place or the spell fails. Only one spell may be cast with a particular rope at a given time, although after the spell ends, a different spell may be cast with that rope. The rope does not need to be laid down in a circular pattern. Although a given circle spell may have a specific way of being broken, all circle spells are broken if the rope is jostled enough to move the ends apart by a character able to cross it. Any circle spell can be suspended by the 4th circle spell Disrupt. It is the PC’s responsibility to know what the Disrupt spell is, how to recognize it, and how to respond to it."
     },
     {
-        "name": "Compulsions",
+        "name": "compulsions",
         "desc": 'Some spells are able to magically compel a character to act in a way the player would prefer they did not. Compulsions can be ignored if they are humiliating or exceedingly difficult commands such as “Kiss my feet” or “Move that wall ten feet to the left.” They also cannot violate OOC mundane laws or ethical codes. They may not force a spellcaster to break their weapon restrictions. Additionally, if the character has been turned undead, they cannot be compelled to communicate knowledge gained before they were made undead.'
     },
     {
-        "name": "Enchanted Beings",
+        "name": "enchanted beings",
         "desc": "All spellcasters, undead, and certain creatures are considered to be enchanted beings. Normal fighters are only enchanted beings if under the effect of certain spells, as per the Undead Caveat. Enchanted beings are affected by a certain number of spells, while non-enchanted beings are not. These spells include Circle of Protection and Ward: Enchanted Beings. By definition, any undead creature is an enchanted being."
     },
     {
-        "name": "Enchanted Items",
+        "name": "enchanted items",
         "desc": "Some spells create or enchant items, or are enhancements that affect players. These spells create magical effects, but are not potent enough for the items bearer or the target of the enchantment to be considered an Enchanted Being, nor is their magic potent enough for the item to be affected by the spell Circle of Protection once they are cast. If the MC of these spells are disenchanted (through the spells Disenchant or the Strange Brew: Potion of Disenchant option) the spell will end, rendering the MC inert. Magic items are not covered by the Enchanted Items Caveat."
     },
     {
-        "name": "Potions",
+        "name": "potions",
         "desc": "A spellcaster who learns Alchemy must have a page in their spellbook listing the sigils that they will use to label potions. Each type of potion they can make, must have a unique, distinguishable sigil. When a potion is made, the spellcaster must put their legible signature, the appropriate sigil, and the date upon the container. Once created, all potions are considered stealable items. A potion can be a represented by a liquid, lotion, elixir, magical food, or anything else, as long as it is safe to be administered in a combat situation. Potions must be directly applied to the recipient. They may not be thrown, dropped, or remotely applied in any manner. The spellcaster need not be present in order to use their potions. \nPotions take a 60 second process called Brewing to be cast. At the beginning of an event, the spellcaster must describe in general terms their process of brewing. An alchemist may brew as many potions as their spells allow during the same 60 seconds. If they wish to create more later, they must begin the process again. If the alchemist is interrupted while brewing, Alchemy points are not lost, but the potions will not be made, and they must start over again in order to make any potions. Although not required, use of additional props or role-playing is encouraged for this process. \nNo potion created by a player can carry over from one event to another; it expires at the end of the event at which it is cast. The PC may choose whether or not to further limit the lifespan of a potion when it is brewed by writing a distinct expiration time among the required spell information on the container. Any potions lacking a specified expiration time last until the end of the event."
     },
     {
-        "name": "Precast",
+        "name": "precast",
         "desc": 'Spells a spellcaster has learned with this caveat allow the spellcaster to begin play with that spell active on them or their gear. This represents the character casting the spell before arriving at the event. This uses up one use of the spell. Any material component that must be on the caster for the spell to work must still be present as per normal (such as sashes). A spell “precast” in this way may only be cast on the caster, and may not be “precast” on anyone else.'
     },
     {
-        "name": "Regeneration",
+        "name": "regeneration",
         "desc": "Some spells grant the ability to regenerate. When this ability is triggered (by death, being wounded, etc.), the target’s wound(s) begin to heal. Until the specified amount of time has passed, this grants no benefit. A blow to any kill location on a dead body will cause a regeneration count to reset no matter where the killing blow was inflicted. Impaling stops regeneration; the count resets when the weapon is removed. Regenerating from death heals all healable wounds on the body. If examined by another person, wounds can be seen to be regenerating. Regeneration that brings a character back from death or soul loss takes 120 seconds. If you are diseased, it takes twice as long to regenerate. You may only be under the effects of one basic regeneration and one advanced regeneration at a time. If more than one source is causing you to regenerate, you may choose which of those spells is causing you to regenerate. A regeneration will only work on someone who is soulless if the spell directly states that it can."
     },
     {
-        "name": "Basic Regeneration",
+        "name": "basic regeneration",
         "desc": "A basic regeneration is a regeneration from death where the regeneration begins when the target dies (or upon casting the spell if the target is already dead). The effect is considered used if the target is raised before their regeneration is complete."
     },
     {
-        "name": "Advanced Regeneration",
+        "name": "advanced regeneration",
         "desc": "An advanced regeneration is a regeneration from death (or soul loss, if specified in the spell description) which begins when the character dies (or is rendered soulless, if it brings a character back from losing their soul). Each time they die, the spellcaster may choose to double the length of their regeneration (to 240 seconds, or 480 if diseased). The regeneration is not considered used if the target is raised before their regeneration is complete."
     },
     {
-        "name": "Spell Sash",
+        "name": "spell sash",
         "desc": "Some spells require a Spell Sash be worn to signify that a specific spell is affecting the wearer. A Spell Sash may be constructed as a sash, a tabard, or a belt favor. A Spell Sash must have the name of the spell clearly written on it. Putting the Spell Sash on the target of the spell, or touching it if the target is already wearing it, is considered to be an Active Component of the spell. A Spell Sash is not stealable and stays on the target of the spell until the spell is ended, at which time it is removed and returned to the spellcaster at the earliest convenient time. Either the spellcaster or the target may choose to end the spell by removing the Spell Sash, and it is also ended early if the Spell Sash is disenchanted. The Spell Sash must be worn in such a way that another player standing 5 feet in front of them can recognise it, and may not be covered unless the majority of their body is covered, such as by wearing a concealing cloak."
     },
     {
-        "name": "Spell Failure",
+        "name": "spell failure",
         "desc": "Some spells have fail conditions in their description. Any spell that fails due to its fail condition expends the casting of that spell."
     },
     {
-        "name": "Suspension",
+        "name": "suspension",
         "desc": "While a spell is suspended, the spell has no effect and does not function until the suspension ends. In addition, a suspended spell can be ended any way it normally could. It is the spellcaster’s responsibility to notify anyone else who is affected by this (such as any players carrying potions that the spellcaster created). When the suspension ends, any spell which has not already ended will resume functioning as per normal."
     },
     {
-        "name": "Undead",
+        "name": "undead",
         "desc": "Some spells will animate a character as undead, allowing them to act as if alive. A person is no longer animated if they are killed, but they are still undead. An undead who is successfully raised (whether they are dead or still animated) is brought back to life and is no longer undead. Animating a character who is undead but slain back to their previous type of undead is referred to as “reanimating”. All undead are affected by the spells: Ward: Enchanted Beings, Ward: Undead, and Circle of Protection (unless the person who animated the undead cast the Circle of Protection). Enchanted beings and items still cannot cross a Circle of Protection that is not their own, regardless of undead status. A PC will remember what happened to them while they were an animated undead. A PC may be animated as undead if they are diseased, but this does not cure them of the disease. Unless otherwise specified, an undead can be harmed as per normal and gains no special protections. If the target is still undead at the end of the event, they are considered dead, and thereforee become soulless and receive a tick. Any spell that ends or is suspended when raised is also ended or suspended while animated as undead."
     },
     {
-        "name": "Thrall",
+        "name": "thrall",
         "desc": "These spells turn a dead (but not soulless) body into an undead creature (referred to as a thrall) that will follow commands given by the spellcaster. What kind of creature the thrall is is left up to the spellcaster, but it must definitely look slightly non-human. The spell must be cast upon a corpse, and the spellcaster must recite the verbal while helping the player into the MC for the spell as appropriate (e.g. tabards or sashes worn correctly). If there is already an appropriate MC on the person, the spellcaster may simply touch the recipient of the spell. When the spell is cast, the spellcaster must explain to the target what it means to be undead. They also must explain the target will obey all direct commands given by the spellcaster that follow the Compulsions Caveat, but the target is otherwise in full possession of their faculties. In addition, the target has the spellcaster’s best interests and intentions in mind. If the spellcaster gives a command that goes against the Compulsions Caveat, the target can ignore the command. These spells simply reanimate a person under the effects of Embrace Death and do not grant any additional powers or control."
     },
     {
-        "name": "Walking Dead",
+        "name": "walking dead",
         "desc": "The spells Beckon Corpse and Zombie Walk enchant a corpse to walk without either returning them to life or being fully animated as undead. A corpse under these effects must move at a walking pace with their hands above their heads to their destination as dictated by the spell. \nThey keep any items on them that they would retain if dragged, whether or not the items are stealable. If anyone directly interferes with their movement or attacks them, they fall to the ground and the effect ends. The Walking Dead cannot take any actions other than walking. They may not attack, search other bodies, cast spells, pick up objects, use magic items, drink potions, or perform any other action aside from movement. The effect does not stop regeneration or other methods of returning to life or becoming fully animated from occurring. Becoming alive or fully animated as undead ends the Walking Dead effect. Corpses under these effects are considered to be undead."
     },
     {
-        "name": "Wards",
+        "name": "wards",
         "desc": "All Ward spells are also Chanting spells. Ward spells affect a specific type of creature listed in the name of the Ward. When active, a Ward spell keeps the spellcaster from being attacked by creatures affected by that spell. A Ward spell affects creatures in front of the spellcaster, limited by a 180 degree hemisphere extending from shoulder to shoulder and outwards from the chest. The Ward will not keep the targeted creature(s) from walking around the spellcaster to attack others nearby. If the Ward affects them, the targeted creature(s) must stay approximately five feet away from the spellcaster, but need not retreat if the spellcaster advances upon them. To cast a Ward, the spellcaster must hold their spell focus out toward the targeted creatures while repeating the verbal. The verbal needs to make it clear what creatures are affected by the Ward. For example, “Stay back undead. Stay back undead. Stay back undead. Shoo,” would be an appropriate verbal for the Ward: Undead spell. This spell will work for as long as the spellcaster holds out the focus and keeps repeating the verbal. The people playing the targeted creatures must be able to hear what the spellcaster is saying, so it is up to the spellcaster to clearly and loudly chant their verbal. While casting this spell, the spellcaster may not attack the targeted creatures. A being that looks like it should be a targeted creature may not be (or may be immune to the Ward). It is still the responsibility of the spellcaster to take any and all weapon blows that hit them, even if the blows are from a creature they believe should be affected by the Ward."
     },
     {
-        "name": "Weapon Calls",
+        "name": "weapon calls",
         "desc": "Each Weapon Call spell has a VC or AC which prepares the weapon with the special combat call, using a casting of the spell. The casting is spent if the blow lands an attack to a legal hit location. Otherwise, upon a parry or a miss, only the preparation is lost and the use remains. If you are unsure whether the blow landed, you must assume that it did. The spellcaster cannot cast a different Weapon Call spell on a currently enchanted weapon until the first spell has been discharged. The spellcaster’s weapon may not be used by anyone else and still retain the enchanted status. If someone other than the spellcaster attacks with the prepared weapon, the preparation is lost and cannot be used unless it is reapplied. \nThe spells that allow special combat calls are Assassin’s Blade, Armor-Piercing Weapon, Disease Weapon, Enchant Weapon, and Create Poison. These spells are mutually exclusive and cannot be cast upon a weapon that already has a separate combat call. Weapon types, such as axes or maces, are not covered by this caveat. None of these spells can be cast upon a Magic Missile or Lightning Bolt."
     }
 ]
